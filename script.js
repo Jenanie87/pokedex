2b4916d624268d8bc09238fb6096ba0ecc1c6fa3
Jenanie87 <107427021+Jenanie87@users.noreply.github.com>
2024-03-20 14:00:30 +0100

    added BigCard as Modal

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 let currentPokemon; // Damit von überall/allen Funktionen auf die Variable zugegriffen werden kann
 let selectionPokemon;
-
-// async function loadPokemon() {
-//     let url = `https://pokeapi.co/api/v2/pokemon/25`;
-//     let response = await fetch(url);
-//     currentPokemon = await response.json();
-//     renderPokemonInfo();
-//     testLoadPokemon();
-// }
-
+let responseAsJSON;
+let fetchedPokemon = [];
 
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon';
     let response = await fetch(url);
-    let responseAsJSON = await response.json();
+    responseAsJSON = await response.json();
+    console.log(responseAsJSON.next);
     selectionPokemon = responseAsJSON.results;
-/*     let currentUrl = selectionPokemon[0].url; */
     renderPokemonInfo();
 }
 
 
 async function renderPokemonInfo() {
     let content = document.querySelector('#content');
+    let mainContainer = document.querySelector('#main_container');
 
     for (let i = 0; i < selectionPokemon.length; i++) {
         const pokemon = selectionPokemon[i];
         let url = pokemon.url;
         let response = await fetch(url);
         currentPokemon = await response.json();
-        console.log(currentPokemon);
+        fetchedPokemon.push(currentPokemon);
+        console.log(currentPokemon.abilities);
 
-        content.innerHTML += generatePokemonInfoInnerHTML(i);
-        renderPokemonElements(i);
-        changeBackgroundColor(i);
+        content.innerHTML += generatePokemonCardsInnerHTML(i);
+                             renderPokemonElements(currentPokemon, i);
+                             changeBackgroundColor(currentPokemon, i);
     }
+    mainContainer.innerHTML += generateButtonNextPokemon();
 }
 
 
-function generatePokemonInfoInnerHTML(i) {
+function generatePokemonCardsInnerHTML(i) {
     return /* HTML */ `
-        <div class="pokemon_card">
+        <div onclick="openBigCard(${i})" class="pokemon_card">
               <div id="top_card${i}" class="top_card pad_section">
             <h2 class="name">${currentPokemon['name']}</h2>
             <div class="id">#${currentPokemon['id']}</div>
@@ -49,16 +45,15 @@ function generatePokemonInfoInnerHTML(i) {
           <div id="pokemon_image${i}" class="pokemon_image">
             <img class="img_pokemon" src="${currentPokemon.sprites.other.dream_world.front_default}" alt="pokemon" />
           </div>
-<!--           <div class="general_information pad_section"></div> -->
+
         </div>`;
 }
 
 
-function renderPokemonElements(j) {
+function renderPokemonElements(array, j) {
     let pokemonElement = document.querySelector(`#pokemon_element${j}`);
-
-    for (let i = 0; i < currentPokemon['types'].length; i++) {
-        const element = currentPokemon['types'][i];
+    for (let i = 0; i < array['types'].length; i++) {
+        const element = array['types'][i];
         pokemonElement.innerHTML += generatePokemonElementsInnerHTML(j, element); 
     }
 }
@@ -68,18 +63,64 @@ function generatePokemonElementsInnerHTML(j, element) {
     return /* HTML */ `
         <p id="text_element${j}" class="text_element">${element.type.name}</p>`;
 }
-// get elements : currentPokemon.types[0].type.name
 
 
-function changeBackgroundColor(i) {
-    addClassColor(i);
+function generateButtonNextPokemon() {
+    return /* HTML */`
+        <div>
+            <button onclick="renderNextPokemonInfo(responseAsJSON)" class="btn_pokemon">Next Pokèmon</button>
+        </div>`;
 }
 
 
-function addClassColor(i) {
-    document.querySelector(`#top_card${i}`).classList.add(`bg_${currentPokemon.types[0].type.name}Pokemon`);
-    document.querySelector(`#pokemon_image${i}`).classList.add(`bg_${currentPokemon.types[0].type.name}Pokemon`);
+function changeBackgroundColor(array, i) {
+    addClassColor(array, i);
+}
+
+
+function addClassColor(array, i) {
+    document.querySelector(`#top_card${i}`).classList.add(`bg_${array.types[0].type.name}Pokemon`);
+    document.querySelector(`#pokemon_image${i}`).classList.add(`bg_${array.types[0].type.name}Pokemon`);
     document.querySelectorAll(`#text_element${i}`).forEach((textElement) => {
-        textElement.classList.add(`bgText_${currentPokemon.types[0].type.name}Pokemon`);
+        textElement.classList.add(`bgText_${array.types[0].type.name}Pokemon`);
     });
+}
+
+
+function openBigCard(i) {
+    changeBigCard();
+    let bg_container = document.querySelector('.bg_container');
+    bg_container.innerHTML = generatePokemonInfosInnerHTML(i);
+                             renderPokemonElements(pokemon, i);
+                             changeBackgroundColor(pokemon, i);
+}
+
+
+function changeBigCard() {
+    let bg_container = document.querySelector('.bg_container');
+    bg_container.classList.toggle('d_none');
+}
+
+
+function doNotClose(event) {
+    event.stopPropagation();
+}
+
+
+function generatePokemonInfosInnerHTML(i) {
+    pokemon = fetchedPokemon[i];
+    return /* HTML */ `
+        <div onclick="doNotClose(event)" class="pokemon_card_big">
+            <div id="top_card${i}" class="top_card pad_section">
+                <h2 class="name">${pokemon['name']}</h2>
+                <div class="id">#${pokemon['id']}</div>
+                <div id="pokemon_element${i}" class="pokemon_element"></div>
+            </div>
+            <div id="pokemon_image${i}" class="pokemon_image">
+                <img class="img_pokemon" src="${pokemon.sprites.other.dream_world.front_default}" alt="pokemon"/>
+            </div>
+            <div class="general_information">
+            <div></div>
+            </div>
+        </div>`;
 }
